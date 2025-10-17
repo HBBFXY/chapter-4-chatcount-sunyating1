6333ba77edb4c467f4c7216998754cc5df49ee60
sunyating1 <1613152158@qq.com>
2025-10-17 11:01:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-line = input()
+line = input().strip()
 letter_count = 0
 digit_count = 0
 space_count = 0
 other_count = 0
 for c in line:
-    if c.isalpha():
+    if ('a' <= c <= 'z') or ('A' <= c <= 'Z'):
         letter_count += 1
     elif c.isdigit():
         digit_count += 1
@@ -12,4 +12,7 @@ for c in line:
         space_count += 1
     else:
         other_count += 1
-print(f"英文字符:{letter_count}\n数字:{digit_count}\n空格:{space_count}\n其他字符:{other_count}")
+print(f"英文字符: {letter_count}")
+print(f"数字: {digit_count}")
+print(f"空格: {space_count}")
+print(f"其他字符: {other_count}")
